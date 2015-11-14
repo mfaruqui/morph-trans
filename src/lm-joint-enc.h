b66446674af4393776917db8d6b8105b0eddca8d
Manaal Faruqui <manaalfar@gmail.com>
2015-11-14 16:58:24 -0500

    corrected the joint encoder + lm files

--- src/lm-joint-enc.h
+++ src/lm-joint-enc.h
@@ -1,5 +1,5 @@
-#ifndef SEP_MORPH_H_
-#define SEP_MORPH_H_
+#ifndef LM_JOINT_ENC_H_
+#define LM_JOINT_ENC_H_
 
 #include "cnn/nodes.h"
 #include "cnn/cnn.h"
@@ -21,21 +21,27 @@ using namespace std;
 using namespace cnn;
 using namespace cnn::expr;
 
+// The class has a shared encoder acorss all morphological types.
+// Other auxiliary units are also shared, except for the decoder and
+// input transofrmation parameters.
 class LMJointEnc {
  public:
-  vector<LSTMBuilder> input_forward, input_backward, output_forward;
-  vector<LookupParameters*> char_vecs;
+  LSTMBuilder input_forward, input_backward;  // Shared encoder
+  vector<LSTMBuilder> output_forward;
+  LookupParameters* char_vecs;  // Shared char vectors
 
   Expression hidden_to_output, hidden_to_output_bias;
-  vector<Parameters*> phidden_to_output, phidden_to_output_bias;
+  Parameters *phidden_to_output, *phidden_to_output_bias;  // Shared
 
   Expression transform_encoded, transform_encoded_bias;
   vector<Parameters*> ptransform_encoded, ptransform_encoded_bias;
+
+  vector<LookupParameters*> eps_vecs; // Not sharing the epsilon vectors
+
+  LookupParameters* lm_pos_weights;
   
   unsigned char_len, hidden_len, vocab_len, layers, morph_len;
-  unsigned max_eps = 5, max_lm_pos_weights = 20;
-  vector<LookupParameters*> eps_vecs;
-  vector<LookupParameters*> lm_pos_weights;
+  unsigned max_lm_pos_weights = 20, max_eps = 5;
 
   LMJointEnc() {}
 
@@ -48,22 +54,20 @@ class LMJointEnc {
 
   void AddParamsToCG(const unsigned& morph_id, ComputationGraph* cg);
 
-  void RunFwdBwd(const unsigned& morph_id, const vector<unsigned>& inputs,
+  void RunFwdBwd(const vector<unsigned>& inputs,
                  Expression* hidden, ComputationGraph *cg);
 
-  void TransformEncodedInput(Expression* encoded_input) const;
-
-  void TransformEncodedInputDuringDecoding(Expression* encoded_input) const;
+  void TransformEncodedInput(Expression* encoded_input) const; 
 
   void ProjectToOutput(const Expression& hidden, Expression* out) const;
 
-  Expression ComputeLoss(const unsigned& morph_id,
-                         const vector<Expression>& hidden_units,
-                         const vector<unsigned>& targets,
-                         LM *lm, ComputationGraph* cg) const;
+  Expression ComputeLoss(const vector<Expression>& hidden_units,
+                         const vector<unsigned>& targets, LM *lm, 
+                         ComputationGraph* cg) const;
 
   float Train(const unsigned& morph_id, const vector<unsigned>& inputs,
-              const vector<unsigned>& outputs, LM* lm, AdadeltaTrainer* ada_gd);
+              const vector<unsigned>& outputs, LM *lm, AdadeltaTrainer* opt,
+              AdadeltaTrainer* shared_opt);
 
   friend class boost::serialization::access;
   template<class Archive> void serialize(Archive& ar, const unsigned int) {
@@ -84,14 +88,14 @@ Expression Softplus(Expression x);
 
 float Softplus(float x);
 
-void
-EnsembleDecode(const unsigned& morph_id, unordered_map<string, unsigned>& char_to_id,
-               const vector<unsigned>& input_ids,
-               vector<unsigned>* pred_target_ids, LM* lm,
-               vector<LMJointEnc*>* ensmb_model);
 
 void Serialize(string& filename, LMJointEnc& model, vector<Model*>* cnn_model);
 
 void Read(string& filename, LMJointEnc* model, vector<Model*>* cnn_model);
+
+void
+EnsembleDecode(const unsigned& morph_id, unordered_map<string, unsigned>& char_to_id,
+               const vector<unsigned>& input_ids, vector<unsigned>* pred_target_ids,
+               LM *lm, vector<LMJointEnc*>* ensmb_model);
 
 #endif
